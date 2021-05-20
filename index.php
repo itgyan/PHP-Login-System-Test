aa14a86b3d7db3a1b8bc9142780699719e282df4
itgyan <itgyanweb@gmail.com>
2021-05-20 17:19:05 +0530

    First Change Commit - CSS Added

--- index.php
+++ index.php
@@ -1,4 +1,48 @@
-<?php
+<!DOCTYPE html>
 
-echo "Hello World";
-?>
+<head>
+    <title>First Project</title>
+
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/css/uikit.min.css" />
+
+
+</head>
+
+<body>
+
+    <div class="uk-section uk-container">
+        <form class="uk-form-stacked">
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                </div>
+            </div>
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Password</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" required="required"  placeholder="Your Password">
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <button class="uk-button uk-button-default" type="submit">
+                    Login
+                </button>
+            </div>
+
+
+        </form>
+    </div>
+
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.6.22/dist/js/uikit-icons.min.js"></script>
+
+</body>
+
+
+</html>
